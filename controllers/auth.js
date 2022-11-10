3dc1701ce6bac7d6d91182252dedc9a020b3d068
JL-OLEMAR <joseluis19963@gmail.com>
2022-11-09 23:57:34 -0500

    feat: Validate required fields for each route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,44 @@
 const { response } = require('express')
+const { validationResult } = require('express-validator')
 
 const createUser = (req, res = response) => {
-  res.json({
+  const { name, email, password } = req.body
+
+  // Error handling
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+  }
+
+  res.status(201).json({
     ok: true,
-    msg: 'SignUp'
+    msg: 'SignUp',
+    name,
+    email,
+    password
   })
 }
 
 const loginUser = (req, res = response) => {
-  res.json({
+  const { email, password } = req.body
+
+  // Error handling
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    })
+  }
+
+  res.status(200).json({
     ok: true,
-    msg: 'login'
+    msg: 'login',
+    email,
+    password
   })
 }
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,16 +4,38 @@
 */
 
 const { Router } = require('express')
+const { check } = require('express-validator')
 const router = Router()
 
 const {
   createUser,
   loginUser,
   RevalidateToken
-} = require('../controllers/auht.js')
+} = require('../controllers/auth')
 
-router.post('/new', createUser)
-router.post('/', loginUser)
+router.post(
+  '/new',
+  [
+    // middlewares
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Password must be six characters long').isLength({
+      min: 6
+    })
+  ],
+  createUser
+)
+router.post(
+  '/',
+  [
+    // middlewares
+    check('email', 'Invalid email').isEmail(),
+    check('password', 'Password must be six characters long').isLength({
+      min: 6
+    })
+  ],
+  loginUser
+)
 router.get('/renew', RevalidateToken)
 
 module.exports = router
