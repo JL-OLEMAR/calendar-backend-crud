9ff14ed3bef2e13268701cff22ae99a11b5f8869
JL-OLEMAR <joseluis19963@gmail.com>
2022-11-11 01:05:52 -0500

    feat: Add login

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,15 +38,43 @@ const createUser = async (req, res = response) => {
   }
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
   const { email, password } = req.body
 
-  res.status(200).json({
-    ok: true,
-    msg: 'login',
-    email,
-    password
-  })
+  try {
+    const user = await UserModel.findOne({ email })
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'User does not exist'
+      })
+    }
+
+    // Confirm passwords, return Boolean
+    const validPwd = bcrypt.compareSync(password, user.password)
+    if (!validPwd) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Incorrect password'
+      })
+    }
+
+    // Generate JWT
+    // TODO: JWT
+
+    res.status(200).json({
+      ok: true,
+      uid: user.id,
+      name: user.name
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact the administrator.'
+    })
+  }
 }
 
 const revalidateToken = (req, res = response) => {
