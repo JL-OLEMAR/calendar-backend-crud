f1239d6c8e6bd2ba8af43d6e00536c8dc55e727b
JL-OLEMAR <joseluis19963@gmail.com>
2022-11-14 21:57:47 -0500

    feat: Update an event

--- controllers/events.js
+++ controllers/events.js
@@ -31,11 +31,43 @@ const createEvent = async (req, res = response) => {
   }
 }
 
-const updateEvent = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'updateEvent'
-  })
+const updateEvent = async (req, res = response) => {
+  const eventId = req.params.id
+  const userUid = req.uid
+
+  try {
+    const event = await EventModel.findById(eventId)
+
+    if (!event) {
+      res.status(404).json({
+        ok: false,
+        msg: 'Event does not exist for that id.'
+      })
+    }
+
+    if (event.user.toString() !== userUid) {
+      res.status(401).json({
+        ok: false,
+        msg: "You don't have privilege to edit this event"
+      })
+    }
+
+    const newUser = { ...req.body, user: userUid }
+    const updatedEvent = await EventModel.findByIdAndUpdate(eventId, newUser, {
+      new: true
+    })
+
+    res.json({
+      ok: true,
+      event: updatedEvent
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Please contact the administrator.'
+    })
+  }
 }
 
 const deleteEvent = (req, res = response) => {
